e8cb48654833dcce7e4d3bd2d3271f85b77785e8
Fernando Rodriguez <fernando@nandotech.com>
2018-04-19 14:30:56 -0400

    updated style for better story reading

--- story.go
+++ story.go
@@ -19,23 +19,63 @@ var defaultHandlerTemplate = `<!DOCTYPE html>
 <html>
 <head>
 <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
-<style>
-body {
-font-family: -apple-system, BlinkMacSystemFont, 'Segoe WPC', 'Segoe UI', 'HelveticaNeue-Light', 'Ubuntu', 'Droid Sans', sans-serif;font-size: 14px;line-height: 1.6;
-}
-</style>
-</head>
+<title>Choose Your Own Adventure!</title>
 <body>
-<h1>{{.Title}}</h1>
-<hr />
-{{range .Paragraphs}}
-<p>{{.}}</p>
-{{end}}
-<ul>
-{{range .Options}}
-<li><a href="/{{.Arc}}">{{.Text}}</a></li>
-{{end}}
-</ul>
+<section class="page">
+  <h1>{{.Title}}</h1>
+  {{range .Paragraphs}}
+	<p>{{.}}</p>
+  {{end}}
+  {{if .Options}}
+	<ul>
+	{{range .Options}}
+	  <li><a href="/{{.Arc}}">{{.Text}}</a></li>
+	{{end}}
+	</ul>
+  {{else}}
+	<h3>The End</h3>
+  {{end}}
+</section>
+<style>
+  body {
+	font-family: helvetica, arial;
+  }
+  h1 {
+	text-align:center;
+	position:relative;
+  }
+  .page {
+	width: 80%;
+	max-width: 500px;
+	margin: auto;
+	margin-top: 40px;
+	margin-bottom: 40px;
+	padding: 80px;
+	background: #FFFCF6;
+	border: 1px solid #eee;
+	box-shadow: 0 10px 6px -6px #777;
+  }
+  ul {
+	border-top: 1px dotted #ccc;
+	padding: 10px 0 0 0;
+	-webkit-padding-start: 0;
+  }
+  li {
+	padding-top: 10px;
+  }
+  a,
+  a:visited {
+	text-decoration: none;
+	color: #6295b5;
+  }
+  a:active,
+  a:hover {
+	color: #7792a2;
+  }
+  p {
+	text-indent: 1em;
+  }
+</style>
 </body>
 </html>`
 
