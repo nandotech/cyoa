85dfee1986795f9c336c4c75bc1b22b05ad1d478
Fernando Rodriguez <fernando@nandotech.com>
2018-04-19 13:34:27 -0400

    working web server implementation renders intro block

---
+++ cmd/cyoaweb/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/nandotech/cyoa"
+)
+
+func main() {
+	filename := flag.String("file", "gopher.json", "the JSON file with the CYOA story")
+	port := flag.Int("port", 3000, "the port to start the web app on")
+	flag.Parse()
+	fmt.Printf("Using the story in %s. \n", *filename)
+
+	f, err := os.Open(*filename)
+	if err != nil {
+		panic(err)
+	}
+
+	story, err := cyoa.JsonStory(f)
+	if err != nil {
+		panic(err)
+	}
+
+	h := cyoa.NewHandler(story)
+	fmt.Printf("starting the server at: %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
+}
